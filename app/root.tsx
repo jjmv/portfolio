f22b8b19b733aeede6fb4a9ad032a4d591406e28
Javier Moreno <jjmv.97@hotmail.com>
2023-07-31 23:20:34 -0600

    feat: added basic color theme

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,5 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction, LoaderArgs } from "@remix-run/node";
-import { json } from "@remix-run/node";
+import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -8,35 +7,73 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useRouteError,
 } from "@remix-run/react";
+import type { PropsWithChildren } from "react";
 
-import { getUser } from "~/session.server";
 import stylesheet from "~/tailwind.css";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
+  { rel: "apple-touch-icon", href: "/apple-touch-icon.png", sizes: "180x180" },
+  {
+    rel: "icon",
+    href: "/favicon-32x32.png",
+    sizes: "32x32",
+    type: "image/png",
+  },
+  {
+    rel: "icon",
+    href: "/favicon-16x16.png",
+    sizes: "16x16",
+    type: "image/png",
+  },
+  { rel: "manifest", href: "/site.webmanifest" },
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
-export const loader = async ({ request }: LoaderArgs) => {
-  return json({ user: await getUser(request) });
-};
-
-export default function App() {
+function Document({
+  children,
+  title = "Javier Moreno",
+}: PropsWithChildren<{ title?: string }>) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" className="h-full bg-white">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
+        <title>{title}</title>
         <Links />
       </head>
       <body className="h-full">
-        <Outlet />
+        {children}
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
+  );
+}
+
+export default function App() {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  const errorMessage = error instanceof Error ? error.message : "Unknown Error";
+
+  return (
+    <Document title="Oops!">
+      <div className="flex h-full w-full items-center justify-center bg-white">
+        <h1>Something went wrong!</h1>
+        <pre>{errorMessage}</pre>
+      </div>
+    </Document>
   );
 }
