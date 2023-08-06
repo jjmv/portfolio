a0ce377c4e85f7446665624a3cfa363fee19d98f
Javier Moreno <jjmv.97@hotmail.com>
2023-08-06 13:27:25 -0600

    feat: add theme context and persisting theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,20 @@
 import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
+  darkMode: "class",
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: "hsl(201, 43%, 45%)",
+        // primary: "hsl(201, 43%, 45%)",
         secondary: "hsl(178, 40%, 47%)",
         tertiary: "hsl(49, 43%, 48%)",
         white: "hsl(60, 100%, 99%)",
         black: "hsl(229, 19%, 22%)",
+      },
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
     },
   },
